31cdde4ab842de96ce6461a6d64f116e2d2d24eb
Hp2325-hp <102241901+Hp2325-hp@users.noreply.github.com>
2022-05-04 11:48:30 +0530

    Update CucumberRunner.java

--- runners/CucumberRunner.java
+++ runners/CucumberRunner.java
@@ -4,7 +4,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 
-@CucumberOptions(features="src/test/java/features/A5_Delete_Lead.feature",glue="steps",monochrome=true
+@CucumberOptions(features="src/test/java/features",glue="steps",monochrome=true
 ,publish=true)
 public class CucumberRunner extends AbstractTestNGCucumberTests{
 
